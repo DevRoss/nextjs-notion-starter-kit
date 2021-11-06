db315c92aaa02f331806f1f3056e2cbdfac33fda
DevRoss <820584458@qq.com>
2021-11-06 17:41:02 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -7,12 +7,15 @@ import * as types from 'lib/types'
 export const PageHead: React.FC<types.PageProps> = ({ site }) => {
   return (
     <Head>
-      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WTF6XMB');`}} />
-      
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-101401601-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'GA_MEASUREMENT_ID');
+      </script>
+
       <meta charSet='utf-8' />
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
       <meta
@@ -30,7 +33,6 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
       <meta name='theme-color' content='#EB625A' />
       <meta property='og:type' content='website' />
     </Head>
-    <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTF6XMB" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
 
   )
 }
