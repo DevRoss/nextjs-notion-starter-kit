d2ee107b3a9e22ac824df53ea2130f3eefa2e888
DevRoss <820584458@qq.com>
2021-11-06 17:00:46 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -8,11 +8,11 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
   return (
     <Head>
       <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WTF6XMB');</script>
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WTF6XMB');`}} />
       <!-- End Google Tag Manager -->
       
       <meta charSet='utf-8' />
@@ -32,5 +32,7 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
       <meta name='theme-color' content='#EB625A' />
       <meta property='og:type' content='website' />
     </Head>
+    <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTF6XMB" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+
   )
 }
