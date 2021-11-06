ad79575c6065840954c7931d2e12fab7b537c467
DevRoss <820584458@qq.com>
2021-11-06 17:01:47 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -77,10 +77,6 @@ export const Footer: React.FC<{
           </a>
         )}
       </div>
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTF6XMB"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
     </footer>
   )
 }
